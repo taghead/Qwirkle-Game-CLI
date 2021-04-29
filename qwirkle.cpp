621f2111d6e62ece9a274d09ec6406e8fe3bd04c
andrew <andrew>
2021-04-29 13:42:01 +1000

    Missing ;

--- qwirkle.cpp
+++ qwirkle.cpp
@@ -94,7 +94,7 @@ LinkedList newTileBag(){
    std::string shapes = {CIRCLE, STAR_4, DIAMOND, SQUARE, STAR_6, CLOVER};
    for ( int c=0; c<AMOUNT_OF_COLORS; c++ ){
       for ( int s=0; s<AMOUNT_OF_SHAPES; s++ ){ 
-         for ( int amount=0; amount<AMOUNT_OF_TILES_PER){
+         for ( int amount=0; amount<AMOUNT_OF_TILES_PER; i++){
             new Tile(colours[c], shapes[s]);         
          }
          // Add tile to node
