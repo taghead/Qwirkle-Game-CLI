a120d6277dc2f058aca8ac3a4a9cf54b909b1fc0
Cameron Tavelli <s3668468@student.rmit.edu.au>
2021-05-14 02:14:22 +1000

    update menu

--- qwirkle.cpp
+++ qwirkle.cpp
@@ -1,14 +1,74 @@
-#include "LinkedList.h"
-
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
 
-#define EXIT_SUCCESS    0
+#include "LinkedList.h"
+#include "TileCodes.h"
+
+#define EXIT_SUCCESS 0
 
 int main(void) {
-   LinkedList* list = new LinkedList();
-   delete list;
+  LinkedList *list = new LinkedList();
+  delete list;
 
-   std::cout << "TODO: Implement Qwirkle!" << std::endl;
+  // GREETING
+  std::cout << "Welcome to Qwirkle!" << std::endl
+  << "-------------------" << std::endl;
 
-   return EXIT_SUCCESS;
+  // MENU
+  std::cout << "Menu" << std::endl
+            << "----" << std::endl
+            << "1. New Game" << std::endl
+            << "2. Load Game" << std::endl
+            << "3. Credits (Show student information)" << std::endl
+            << "4. Quit" << std::endl;
+
+  // USER CHOICE
+  while (!std::cin.eof()) {
+    std::cout << std::endl << "> ";
+    
+    char choice;
+    std::cin >> choice;
+
+    std::cout << std::endl;
+    if (choice == '1') {
+      std::cout << "NEW GAME";
+    }
+    else if (choice == '2') {
+      std::cout << "LOAD GAME";
+    }
+    else if (choice == '3') {
+      std::cout << "----------------------------------" << std::endl
+                << "Name: James Parsell" << std::endl
+                << "Student ID: s3599751" << std::endl
+                << "Email: s3599751@student.rmit.edu.au" << std::endl
+                << std::endl
+                << "Name: Andrew Chander" << std::endl
+                << "Student ID: s3666434" << std::endl
+                << "Email: s3666434@student.rmit.edu.au" << std::endl
+                << std::endl
+                << "Name: Cameron Tavelli" << std::endl
+                << "Student ID: s3668468" << std::endl
+                << "Email: s3668468@student.rmit.edu.au" << std::endl
+                << std::endl
+                << "Name: Siang Hii" << std::endl
+                << "Student ID: s3668877" << std::endl
+                << "Email: s3668877@student.rmit.edu.au" << std::endl
+                << "----------------------------------" << std::endl;
+    }
+    else if (choice == '4') {
+      std::cout << "Goodbye!" << std::endl;
+      exit(0);
+    }
+    else if (std::cin.eof()) {
+      std::cout << std::endl << "Goodbye!" << std::endl;
+      exit(0);
+    }
+    else {
+      std::cout << "Invalid Input" << std::endl;
+    }
+  }
+
+  return EXIT_SUCCESS;
 }
