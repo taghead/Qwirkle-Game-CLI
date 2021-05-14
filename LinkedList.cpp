01f6ded044e66c3b146afa668f21430d4bd76850
Cameron Tavelli <s3668468@student.rmit.edu.au>
2021-05-14 17:06:01 +1000

    linkedlist completed. assuming ADT structure, disregard errors.

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,10 +1,149 @@
 #include "LinkedList.h"
 
 LinkedList::LinkedList() {
-   head = nullptr;
-
-   // TODO
+  head = nullptr;
+  tail = nullptr;
+  size = 0;
 }
 
 LinkedList::~LinkedList() {
+  // delete head
+  // delete tail
+  // clear();
+}
+
+void LinkedList::addFront(Tile *tile) {
+  Node *tmp = new Node(tile, head, nullptr);
+  if (head == nullptr) {
+    head = tmp;
+    tail = tmp;
+  }
+  else {
+    head = tmp;
+  }
+  size++;
+}
+
+void LinkedList::addBack(Tile *tile) {
+  Node *tmp = new Node(tile, nullptr, tail);
+  if (head == nullptr) {
+    head = tmp;
+    tail = tmp;
+    tmp = nullptr;
+  }
+  else {
+    tail->next = tmp;
+    tail = tmp;
+  }
+  size++;
+}
+
+void LinkedList::deleteFront() {
+  Node *tmp = head;
+  head = head->next;
+  delete tmp;
+  size--;
+}
+
+void LinkedList::deleteBack() {
+  Node *tmp = tail;
+  tail = tail->prev;
+  delete tmp;
+  size--;
+}
+
+void LinkedList::deleteAt(int pos) {
+  if (pos > size) {
+    return;
+  }
+  if (pos == 0) {
+    deleteFront();
+  }
+  else if (pos == size - 1) {
+    deleteBack();
+  }
+  else {
+    Node *curr = head;
+    for (int i = 0; i < pos; i++) {
+      curr = curr->next;
+    }
+    curr->prev->next = curr->next;
+    curr->next->prev = curr->prev;
+    delete curr;
+    size--;
+  }
+}
+
+int LinkedList::getSize() {
+  return size;
+}
+
+Node *LinkedList::getNode(Tile *tile) {
+  Node *foundNode = nullptr;
+  Node *curr = head;
+  while (curr != nullptr && foundNode == nullptr) {
+    if (curr->getTile() == tile) {
+      foundNode = curr;
+    }
+    curr = curr->next;
+  }
+  return foundNode;
+}
+
+Node *LinkedList::getNodeAt(int index) {
+  Node *foundNode = nullptr;
+  Node *curr = head;
+  int i = 0;
+  while (i < index) {
+    curr = curr->next;
+    i++;
+  }
+  foundNode = curr;
+  return foundNode;
+}
+
+Tile *LinkedList::getTileAt(int index) {
+  int i = 0;
+  Node *curr = head;
+  Tile *tile;
+  while (i < index) {
+    curr = curr->next;
+    i++;
+  }
+  tile = curr->getTile();
+  return tile;
+}
+
+std::string LinkedList::toStringBoard() {
+  std::string listString;
+  Node *curr = head;
+  if (size > 0) {
+    for (int i = 0; i < size; i++) {
+      listString += (curr->getTile()->toString() + " ");
+      curr = curr->next;
+    }
+  }
+  return listString;
+}
+
+std::string LinkedList::toStringHand() {
+  std::string listString;
+  Node *curr = head;
+  if (size > 0) {
+    for (int i = 0; i < size - 1; i++) {
+      listString += (curr->getTile()->toString() + ",");
+      curr = curr->next;
+    }
+    listString += curr->getTile()->toString();
+  }
+  return listString;
+}
+
+void LinkedList::clear() {
+  while (this->head != nullptr) {
+    Node *toDelete = this->head;
+    head = toDelete->next;
+    delete toDelete;
+  }
+  tail = nullptr;
 }
--- LinkedList.h
+++ LinkedList.h
@@ -5,12 +5,34 @@
 
 class LinkedList {
 public:
+  LinkedList();
+  ~LinkedList();
 
-   LinkedList();
-   ~LinkedList();
+  // Add
+  void addFront(Tile *tile);
+  void addBack(Tile *tile);
+
+  // Delete
+  void deleteFront();
+  void deleteBack();
+  void deleteAt(int pos);
+
+  // Getters
+  int getSize();
+  Tile *getTileAt(int index);
+  Node *getNode(Tile *tile);
+  Node *getNodeAt(int index);
+
+  // To Strings
+  std::string toStringBoard();
+  std::string toStringHand();
 
 private:
-   Node* head;
+  Node *head;
+  Node *tail;
+  int size;
+  
+  void clear();
 };
 
 #endif // ASSIGN2_LINKEDLIST_H
