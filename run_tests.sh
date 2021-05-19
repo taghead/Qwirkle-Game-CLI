2721e8f43e361a88b23d65766956a67ea50b5285
Andrew <s3666434@student.rmit.edu.au>
2021-05-19 17:30:22 +1000

    Color coded run_test.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
+
+## Use latest compilation
 make clean; make;
+
+## Define diff and options
+DIFF="/usr/bin/diff"
+DIFF_OPTS="--brief --report-identical-files -w"
+
+## Define colors
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+## Check for colordiff
+if [[ -f /usr/bin/colordiff ]]; then
+  DIFF="/usr/bin/colordiff"
+fi
+
+## Run tests
 for f in ./Tests/*.input; 
 do 
-  echo "Testing ${f%??????}";
-  # ./qwirkle < $f;
-  ./qwirkle < $f > ${f%??????}.gameout
-  # ./qwirkle < $f > ${f%??????}.output
-  diff --brief --report-identical-files -w ${f%??????}.output ${f%??????}.gameout
-  if [[ -f ${f%??????}.expsave ]]; then
-    diff --brief --report-identical-files -w -y ${f%??????}.expsave ${f%??????}.save
+  # Remove file extension
+  FILENAME="${f%??????}"
+
+  # Run tests
+  echo -e "${YELLOW}Testing $FILENAME ${NC}";
+  ./qwirkle < $f > $FILENAME.gameout
+  $DIFF $DIFF_OPTS $FILENAME.output $FILENAME.gameout
+  if [[ -f $FILENAME.expsave ]]; then
+    $DIFF $DIFF_OPTS $FILENAME.expsave $FILENAME.save
   fi
   echo -e "\n";
 done
